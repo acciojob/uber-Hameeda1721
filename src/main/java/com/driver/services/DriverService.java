6281977415fc5136a82c940879d06e166e402d27
Hameeda1721 <hameedaravikala69@gmail.com>
2023-01-22 15:44:27 +0530

    uber

--- src/main/java/com/driver/services/DriverService.java
+++ src/main/java/com/driver/services/DriverService.java
@@ -7,4 +7,6 @@ public interface DriverService {
 		public void register(String mobile, String password);
 		public void removeDriver(int driverId);
 		public void updateStatus(int driverId);
+
+    void registerDriver(String mobile, String password);
 }
